af65b304b657a0fce12c5b4babbdd91ab0a776ce
SoyAngelica <soyangelica.aguilar@gmail.com>
2019-08-19 21:12:19 -0500

    Sections & icons

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,11 +9,69 @@ export default class Intro extends Component {
             <div className="overlay">
                 <div className="container-fluid">
                   <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="col-md-10 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1><br />Hola! Soy Angélica</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1><br />Acerca de mi</h1>
+                          <blockquote>
+                            <p>Tengo habilidad para aprender nuevas tecnologías y solucionar problemáticas. Fanática de la organización.</p>
+                            <p>Como freelancer trato de forma directa y más conectada con mis clientes, mientras disfruto trabajando en lo que más me gusta.</p>
+                            <p>Me encanta viajar, andar en bici, instagram, la música y el té.</p>
+                          </blockquote>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>      
+            </div>
+        </section>
+        <section id="colorlib-hero" className="js-fullheight" data-section="skills">          
+            <div className="overlay">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-10 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Mis skills<br /></h1>
+                          <blockquote>
+                            <p></p>
+                          </blockquote>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>      
+            </div>
+        </section>
+        <section id="colorlib-hero" className="js-fullheight" data-section="projects">          
+            <div className="overlay">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-10 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Mi Trabajo<br /></h1>
+                          <blockquote>
+                            <p></p>
+                          </blockquote>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>      
+            </div>
+        </section>
+        <section id="colorlib-hero" className="js-fullheight" data-section="contact">          
+            <div className="overlay">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-10 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Contacto<br /></h1>
+                          <blockquote>
+                            <p></p>
+                          </blockquote>
                         </div>
                       </div>
                     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/soyangelica.jpeg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/IMG_6728P.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Soy Angélica</a></h1>
               <p>+52 55 6972 7405</p>
               <p>soyangelica.aguilar@gmail.com</p>
@@ -16,28 +16,26 @@ export default class Sidebar extends Component {
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
                 <ul>
-                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
-                  <li><a href="#about" data-nav-section="about">About</a></li>
-                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
-                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                  <li className="active"><a href="#home" data-nav-section="home">Acerca de mi</a></li>
+                  <li><a href="#skills" data-nav-section="skills">Skills</a></li>
+                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#blog" data-nav-section="blog">Blog</a></li>
+                  <li><a href="#contact" data-nav-section="contact">Contacto</a></li>
                 </ul>
               </div>
             </nav>
             <nav id="colorlib-main-menu">
-              <ul>
-                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
+              <ul id="social">
+                <li><a href="https://twitter.com/sisoyangelica" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://github.com/soyAngelica/" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://www.instagram.com/angelica.colores/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                {/* <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li> */}
               </ul>
             </nav>
             <div className="colorlib-footer">
               <p><small>
-                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
-                  Theme Inspired on <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>
+                  {/* Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br> */}
+                  {/* Theme Inspired on <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> */}
               </small></p>
               <p><small>
               </small></p>
